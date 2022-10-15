f230d709daeb286f2877eb4799c5acbf36a2c2a6
JakubZ21 <58000694+JakubZ21@users.noreply.github.com>
2022-10-15 13:08:22 +0200

    other

--- server/server.js
+++ server/server.js
@@ -3,117 +3,74 @@ const express = require('express')
 const { Connection, Request } = require("tedious");
 const app = express()
 
-const config = {
-  authentication: {
-    options: {
-      userName: "quiz_reader",
-      password: "wsmsjzOu!z"
-    },
-    type: "default"
-  },
-  server: "wsmsjz-learning-helper-tool-sqlsrv-dev.database.windows.net",
-  options: {
-    database: "learning_helper_tool_sqldb_dev", //update me
-    encrypt: true
-  }
-};
-
-var jsonArray = [];
-var connection = new Connection(config)
-
-
-connection.on('connect', function(err)
-{
-  if(err){
-    console.log(err)
-  }else
-  {
-    console.log("connected")
-    const resp = execSQL();
-    console.log(resp[1]+"here")
-  }
-})
-
-connection.connect()
-
-// res.json([])
-
-
-app.get("/api", (req, res) => {
-  res.json({ "users": ["userOne", "userTwo", "userThree"] })
-})
-//Uzywana jest biblioteka tedious ktora sluzy do polaczenia z azure sql
 app.get("/testconn", (req, res) => {
 
+  const config = {
+    authentication: {
+      options: {
+        userName: "quiz_reader",
+        password: "wsmsjzOu!z"
+      },
+      type: "default"
+    },
+    server: "wsmsjz-learning-helper-tool-sqlsrv-dev.database.windows.net",
+    options: {
+      database: "learning_helper_tool_sqldb_dev", //update me
+      encrypt: true
+    }
+  };
   
-
-
-  // Create connection to database
-  // getQuestData(res);
-
-  // console.log(jsonArray)
-
-  // Attempt to connect and execute queries if connection goes through
-  // This returns 3 rows
-
+  var jsonArray = [];
+  var connection = new Connection(config)
   
+  
+  
+  
+  connection.connect((err) => {
+    if (err) {
+      console.log('Connection Failed');
+      throw err;
+    }
+  
+    executeStatement();
+  });
+  
+  function executeStatement() {
+    const request = new Request('select * from questions', (err, rowCount) => {
+      if (err) {
+        throw err;
+      }
+  
+      console.log('DONE!');
+      connection.close();
+    });
+  
+    // Emits a 'DoneInProc' event when completed.
+    request.on('row', (columns) => {
+      columns.forEach((column) => {
+        if (column.value === null) {
+          console.log('NULL');
+        } else {
+          console.log(column.value);
+        }
+      });
+    });
+  
+    request.on('done', (rowCount) => {
+      console.log('Done is called!');
+    });
+  
+    request.on('doneInProc', (rowCount, more) => {
+      console.log(rowCount + ' rows returned');
+    });
+  
+    // In SQL Server 2000 you may need: connection.execSqlBatch(request);
+    connection.execSql(request);
+  }
+  
+  res.json({ "users": ["userOne", "userTwo", "userThree"] })
 })
 
-app.listen(5000, () => { console.log("Server started on port 5000") })
 
 
-function execSQL()
-{
 
-  request = new Request(
-    //`SELECT * FROM SampleTable`,
-    "SELECT * FROM questions",
-    (err) => {
-      if (err) {
-        console.error(err.message);
-      }     
-    }
-  );
-
-  connection.execSql(request)
-  var counter = 1 
-  resp = {}
-  request.on('row', function(columns)
-  {
-    resp[counter] = {}
-    columns.forEach(function(column)
-    {
-      resp[counter][column.metadata.colName] = column.value
-    });
-    counter += 1;
-  })
-  request.on('done', 
-  function (rowCount, more, rows) {console.log("done is called")});
-  
-  return resp
-}
-
-function getQuestData(res) {
-
-  
-
-  connection.connect(function (err) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    else {
-      
-      request.on("row", columns => {
-        var rowObject = {};
-        columns.forEach(column => {
-          rowObject[column.metadata.colName] = column.value
-        });
-        jsonArray.push(rowObject);
-      });
-
-      connection.execSql(request);
-      res.json(jsonArray)
-    }
-  })
-}
