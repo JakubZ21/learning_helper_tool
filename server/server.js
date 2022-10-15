afc74231456a6d10a20e99aa5e682201c21e03e4
JakubZ21 <58000694+JakubZ21@users.noreply.github.com>
2022-10-15 12:01:40 +0200

    First attempt

--- server/server.js
+++ server/server.js
@@ -1,34 +1,56 @@
 const { json } = require('express')
 const express = require('express')
+const { Connection, Request } = require("tedious");
 const app = express()
+var jsonArray = [];
+var connection;
 
-app.get("/api", (req,res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+app.get("/api", (req, res) => {
+  res.json({ "users": ["userOne", "userTwo", "userThree"] })
 })
 //Uzywana jest biblioteka tedious ktora sluzy do polaczenia z azure sql
-app.get("/testconn", (req,res) => {
+app.get("/testconn", async (req, res) => {
 
-    const { Connection, Request } = require("tedious");
+  
+  // Create connection to database
+  getQuestData(res);
 
-    // Create connection to database
-    const config = {
-      authentication: {
-        options: {
-          userName: "quiz_reader", 
-          password: "wsmsjzOu!z" 
-        },
-        type: "default"
-      },
-      server: "wsmsjz-learning-helper-tool-sqlsrv-dev.database.windows.net", 
+  // console.log(jsonArray)
+
+  // Attempt to connect and execute queries if connection goes through
+  // This returns 3 rows
+
+  
+})
+app.listen(5000, () => { console.log("Server started on port 5000") })
+
+
+async function getQuestData(res) {
+
+  const config = {
+    authentication: {
       options: {
-        database: "learning_helper_tool_sqldb_dev", //update me
-        encrypt: true
-      }
-    };
+        userName: "quiz_reader",
+        password: "wsmsjzOu!z"
+      },
+      type: "default"
+    },
+    server: "wsmsjz-learning-helper-tool-sqlsrv-dev.database.windows.net",
+    options: {
+      database: "learning_helper_tool_sqldb_dev", //update me
+      encrypt: true
+    }
+  };
 
-    var jsonArray = [];
-    const connection = new Connection(config);
-    const request = new Request(
+  connection = new Connection(config)
+
+  await connection.connect(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    else {
+      const request = new Request(
         //`SELECT * FROM SampleTable`,
         "SELECT * FROM questions",
         (err, rowCount) => {
@@ -36,30 +58,20 @@ app.get("/testconn", (req,res) => {
             console.error(err.message);
           } else {
             console.log(`${rowCount} row(s) returned`);
+            // res.json(jsonArray)
           }
         }
       );
-      
       request.on("row", columns => {
         var rowObject = {};
         columns.forEach(column => {
           rowObject[column.metadata.colName] = column.value
         });
         jsonArray.push(rowObject);
-        console.log(jsonArray)
       });
-      //console.log(jsonArray)
-      
-    // Attempt to connect and execute queries if connection goes through
-    // This returns 3 rows
-    connection.connect(function (err) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        connection.execSql(request);
-        
-    })
-    res.json(connection.execSql(request))
-})
-app.listen(5000, () => {console.log("Server started on port 5000")})
+
+      connection.execSql(request);
+      res.json(jsonArray)
+    }
+  })
+}
