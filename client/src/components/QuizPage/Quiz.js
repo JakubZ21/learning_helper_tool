13343b13bb88f1b74bec5d7cc7a8cab2e46b275f
sero1910 <pcz.pracownik1@outlook.com>
2022-10-29 11:08:21 +0200

    updates

--- client/src/components/QuizPage/Quiz.js
+++ client/src/components/QuizPage/Quiz.js
@@ -1,183 +1,84 @@
 import './Quiz.css';
 import BackGround from '../UI/BackGround';
 import { useState, useEffect } from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
-
-
 const Quiz = () => {
-
 	const [index, setIndex] = useState(0);
 	const [isActive, setIsActive] = useState(true);
-	let history = useHistory()
+	let history = useHistory();
 
 	const handleClick = () => {
 		setIsActive(false);
 	};
 
-	
-const API_ENDPOINT = "http://localhost:5000/";
-const url = "http://localhost:5000/questions/get10random";
+	const API_ENDPOINT = 'http://localhost:5000/';
+	const url = 'http://localhost:5000/questions/get10random';
 
 	const [waiting, setWaiting] = useState(true);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
 	const [questionsFetched, setQuestionsFetched] = useState([]);
-	const [correctAnswer, setCorrect] = useState()
-	const [answersState, setAnswers] = useState([])
+	const [correctAnswer, setCorrect] = useState();
+	const [answersState, setAnswers] = useState([]);
 
-	const [questionContent, setContent] = useState("") 
+	const [questionContent, setContent] = useState('');
 
-	
+	const fetchQuestions = async (API_ENDPOINT) => {
+		const response = await axios(url).catch((err) => console.log(err));
+		if (response) {
+			const data = response.data;
+			if (data.length > 0) {
+				setQuestionsFetched(response.data);
+				console.log(questionsFetched);
+				setLoading(false);
+				setWaiting(false);
+				setError(false);
+			} else {
+				setWaiting(true);
+				setError(true);
+			}
+		} else {
+			setWaiting(true);
+		}
+	};
 
+	useEffect(() => {
+		fetchQuestions();
+	}, []);
 
-const fetchQuestions = async (API_ENDPOINT) =>
-{
-	const response = await axios(url).catch((err) => console.log(err));
-    if (response) {
-      const data = response.data;
-      if (data.length > 0) {
-        setQuestionsFetched(response.data);
-		console.log(questionsFetched)
-        setLoading(false);
-        setWaiting(false);
-        setError(false);
-		
-
-      } else {
-        setWaiting(true);
-        setError(true);
-
-      }
-    } else {
-      setWaiting(true);
-
-    }
-}	
-
-useEffect(() =>
-{
-	fetchQuestions();
-}, [])
-
-useEffect(() =>
-{
-	if (typeof questionsFetched[index] !== "undefined")
-	{
-	setContent(questionsFetched[index].question_content)
-	let answers = []
-	answers.push(questionsFetched[index].answer_1)
-	answers.push(questionsFetched[index].answer_2)
-	answers.push(questionsFetched[index].answer_3)
-	answers.push(questionsFetched[index].answer_correct)
-	answers.sort(() => Math.random() - 0.5)
-	setCorrect(answers.findIndex(element => element === questionsFetched[index].answer_correct))
-	setAnswers(answers)
-	// console.log(answers)
-	}
-}, [questionsFetched[index], index])
-
-	// const questions = [
-	// 	{
-	// 		id: 1,
-	// 		question: 'W Której wsi rozgrywają się losy bohaterów serialu "Ranczo"?',
-
-	// 		answer: [
-	// 			{
-	// 				answerPrefix: 'A',
-	// 				answerText: 'Wilkowyje',
-	// 				answerIsTrue: true,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'B',
-	// 				answerText: 'Nowa wieś',
-	// 				answerIsTrue: false,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'C',
-	// 				answerText: 'Stara wieś',
-	// 				answerIsTrue: false,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'D',
-	// 				answerText: 'Lipany',
-	// 				answerIsTrue: false,
-	// 			},
-	// 		],
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		question: 'Który piłkarz zdobył w tym roku złotą piłkę?',
-
-	// 		answer: [
-	// 			{
-	// 				answerPrefix: 'A',
-	// 				answerText: 'R.Lewandowski',
-	// 				answerIsTrue: false,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'B',
-	// 				answerText: 'C.Ronaldo',
-	// 				answerIsTrue: false,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'C',
-	// 				answerText: 'L.Messi',
-	// 				answerIsTrue: false,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'D',
-	// 				answerText: 'K.Benzema',
-	// 				answerIsTrue: true,
-	// 			},
-	// 		],
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		question: 'Jak miał na imię Papkin w "Zemście?',
-
-	// 		answer: [
-	// 			{
-	// 				answerPrefix: 'A',
-	// 				answerText: 'Michał',
-	// 				answerIsTrue: false,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'B',
-	// 				answerText: 'Piotr',
-	// 				answerIsTrue: false,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'C',
-	// 				answerText: 'Józef',
-	// 				answerIsTrue: true,
-	// 			},
-	// 			{
-	// 				answerPrefix: 'D',
-	// 				answerText: 'Jan',
-	// 				answerIsTrue: false,
-	// 			},
-	// 		],
-	// 	},
-	// ];
-	
+	useEffect(() => {
+		if (typeof questionsFetched[index] !== 'undefined') {
+			setContent(questionsFetched[index].question_content);
+			let answers = [];
+			answers.push(questionsFetched[index].answer_1);
+			answers.push(questionsFetched[index].answer_2);
+			answers.push(questionsFetched[index].answer_3);
+			answers.push(questionsFetched[index].answer_correct);
+			answers.sort(() => Math.random() - 0.5);
+			setCorrect(
+				answers.findIndex(
+					(element) => element === questionsFetched[index].answer_correct
+				)
+			);
+			setAnswers(answers);
+			// console.log(answers)
+		}
+	}, [questionsFetched[index], index]);
 
 	const nextQuestion = () => {
 		setIndex((oldIndex) => {
 			const index = oldIndex + 1;
 			if (index > questionsFetched.length - 1) {
 				console.log('Koniec gry');
-				history.push('/')
+				history.push('/');
 				return 0;
 			} else {
 				return index;
 			}
 		});
 	};
-
-	
-
 
 	return (
 		<div>
@@ -187,9 +88,8 @@ useEffect(() =>
 					<h2 className='question'>{questionContent}</h2>
 
 					{answersState.map((answer, index) => (
-						//onClick -- index++
 						<div className='choice-container'>
-							<p className='choice-prefix'>{index+1}</p>
+							<p className='choice-prefix'>{index + 1}</p>
 							<p
 								className='choice-text'
 								style={{
@@ -208,8 +108,7 @@ useEffect(() =>
 				</div>
 			</div>
 		</div>
-		
-		
+
 		// Statycznie
 		// <div>
 		// 	<BackGround />
@@ -243,3 +142,88 @@ useEffect(() =>
 };
 
 export default Quiz;
+
+///Dane Statyczne
+// const questions = [
+// 	{
+// 		id: 1,
+// 		question: 'W Której wsi rozgrywają się losy bohaterów serialu "Ranczo"?',
+
+// 		answer: [
+// 			{
+// 				answerPrefix: 'A',
+// 				answerText: 'Wilkowyje',
+// 				answerIsTrue: true,
+// 			},
+// 			{
+// 				answerPrefix: 'B',
+// 				answerText: 'Nowa wieś',
+// 				answerIsTrue: false,
+// 			},
+// 			{
+// 				answerPrefix: 'C',
+// 				answerText: 'Stara wieś',
+// 				answerIsTrue: false,
+// 			},
+// 			{
+// 				answerPrefix: 'D',
+// 				answerText: 'Lipany',
+// 				answerIsTrue: false,
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 2,
+// 		question: 'Który piłkarz zdobył w tym roku złotą piłkę?',
+
+// 		answer: [
+// 			{
+// 				answerPrefix: 'A',
+// 				answerText: 'R.Lewandowski',
+// 				answerIsTrue: false,
+// 			},
+// 			{
+// 				answerPrefix: 'B',
+// 				answerText: 'C.Ronaldo',
+// 				answerIsTrue: false,
+// 			},
+// 			{
+// 				answerPrefix: 'C',
+// 				answerText: 'L.Messi',
+// 				answerIsTrue: false,
+// 			},
+// 			{
+// 				answerPrefix: 'D',
+// 				answerText: 'K.Benzema',
+// 				answerIsTrue: true,
+// 			},
+// 		],
+// 	},
+// 	{
+// 		id: 3,
+// 		question: 'Jak miał na imię Papkin w "Zemście?',
+
+// 		answer: [
+// 			{
+// 				answerPrefix: 'A',
+// 				answerText: 'Michał',
+// 				answerIsTrue: false,
+// 			},
+// 			{
+// 				answerPrefix: 'B',
+// 				answerText: 'Piotr',
+// 				answerIsTrue: false,
+// 			},
+// 			{
+// 				answerPrefix: 'C',
+// 				answerText: 'Józef',
+// 				answerIsTrue: true,
+// 			},
+// 			{
+// 				answerPrefix: 'D',
+// 				answerText: 'Jan',
+// 				answerIsTrue: false,
+// 			},
+// 		],
+// 	},
+// ];
